72aac6f827de1d4fc18e42f06de8cf464bc79e55
marcinzh <marcinzh1@yahoo.com>
2019-12-11 21:02:29 +0100

    Interpreter has, and no longer is, a Monad

--- modules/core/src/main/scala/turbolift/abstraction/handlers/Interpreter.scala
+++ modules/core/src/main/scala/turbolift/abstraction/handlers/Interpreter.scala
@@ -5,72 +5,68 @@ import turbolift.abstraction.!!
 import turbolift.abstraction.ComputationCases._
 
 
-sealed trait Interpreter[M[+_], +U] extends MonadPar[M] {
+sealed trait Interpreter[M[+_], +U] {
   def dispatch[A, Z <: Signature](effectId: AnyRef, op: Z => Z#Op[A]): M[A]
   def dispatchFail: M[Nothing]
 
-  final def push[T[_[+_], +_], V](ih: PrimitiveHandler[T]): Interpreter[T[M, +?], U with V] =
-    new StackedHandler[M, U, T, V](ih: PrimitiveHandler[T], this)
+  final def push[T[_[+_], +_], V](primitive: PrimitiveHandler[T]): Interpreter[T[M, +?], U with V] =
+    new Push[M, U, T, V](primitive: PrimitiveHandler[T], this)
 
-  implicit def theMonad: MonadPar[M] = this
+  val theMonad: MonadPar[M]
 
   final def run_![A](ua: A !! U): M[A] = loop(ua)
 
-  private def loop[A](ua: A !! U): M[A] =
+  private def loop[A](ua: A !! U): M[A] = {
+    // implicit def M: MonadPar[M] = theMonad
     ua match {
-      case Pure(a) => pure(a)
+      case Pure(a) => theMonad.pure(a)
       case FlatMap(ux, k) => ux match {
         case Pure(x) => loop(k(x))
         case FlatMap(uy, j) => loop(FlatMap(uy, (y: Any) => FlatMap(j(y), k)))
         case _ => loop(ux).flatMap(x => loop(k(x)))
       }
-      case ZipPar(uy, uz) => zipPar(loop(uy), loop(uz))
+      case ZipPar(uy, uz) => theMonad.zipPar(loop(uy), loop(uz))
       case Dispatch(id, op) => dispatch(id, op)
       case Fail => dispatchFail
       case HandleInScope(uy, ph) =>
         val h2 = push[ph.Trans, ph.Effects](ph.primitive)
         ph.gimmick(h2.loop(uy))
+    }
   }
 }
+
 
 object Interpreter {
   val pure: Interpreter[Trampoline, Any] = apply(TrampolineInstances.monad)
   val pureStackUnsafe: Interpreter[Identity, Any] = apply(MonadPar.identity)
 
-  def apply[M[+_]](implicit M: MonadPar[M]): Interpreter[M, Any] = new Unhandled[M] {
-    def pure[A](a: A): M[A] = M.pure(a)
-    def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] = M.flatMap(ma)(f)
-    def zipPar[A, B](ma: M[A], mb: M[B]): M[(A, B)] = M.zipPar(ma, mb)
-  }
-
-  trait Unhandled[M[+_]] extends Interpreter[M, Any] {
-    def dispatch[A, Z <: Signature](effectId: AnyRef, op: Z => Z#Op[A]): M[A] = unhandled(effectId.toString)
-    def dispatchFail: M[Nothing] = unhandled("Fail")
-    private def unhandled(msg: String) = sys.error(s"Unhandled effect: $msg")
+  def apply[M[+_]](implicit M: MonadPar[M]): Interpreter[M, Any] = new Interpreter[M, Any] {
+    override val theMonad: MonadPar[M] = M
+    override def dispatch[A, Z <: Signature](effectId: AnyRef, op: Z => Z#Op[A]): M[A] = unhandled(effectId.toString)
+    override def dispatchFail: M[Nothing] = unhandled("Fail")
+    def unhandled(msg: String) = sys.error(s"Unhandled effect: $msg")
   }
 }
 
 
-private[abstraction] final class StackedHandler[M[+_], U, T[_[+_], +_], V](
+private[abstraction] final class Push[M[+_], U, T[_[+_], +_], V](
   val primitive: PrimitiveHandler[T],
   val next: Interpreter[M, U]
 ) extends Interpreter[T[M, +?], U with V] {
-  private implicit def M: MonadPar[M] = next
-  private type TM[+A] = T[M, A]
+  type TM[+A] = T[M, A]
 
-  def pure[A](a: A): TM[A] = primitive.lift(next.pure(a))
-  def flatMap[A, B](ma: TM[A])(f: A => TM[B]): TM[B] = primitive.flatMap(ma)(f)
-  def zipPar[A, B](ma: TM[A], mb: TM[B]): TM[(A, B)] = primitive.zipPar(ma, mb)
+  implicit def nextMonad: MonadPar[M] = next.theMonad
+  override val theMonad: MonadPar[TM] = primitive.makeMonad[M]
 
   private val decode = primitive.decode[M]
 
-  def dispatch[A, Z <: Signature](effectId: AnyRef, op: Z => Z#Op[A]): TM[A] =
+  override def dispatch[A, Z <: Signature](effectId: AnyRef, op: Z => Z#Op[A]): TM[A] =
     if (effectId eq primitive.effectId)
       op.asInstanceOf[primitive.Encoded[M, A]](decode)
     else
       primitive.lift(next.dispatch(effectId, op))
 
-  def dispatchFail: TM[Nothing] =
+  override def dispatchFail: TM[Nothing] =
     if(primitive.isFilterable)
       FailSig.encodeFail.asInstanceOf[primitive.Encoded[M, Nothing]](decode)
     else
--- modules/core/src/main/scala/turbolift/abstraction/handlers/PrimitiveHandler.scala
+++ modules/core/src/main/scala/turbolift/abstraction/handlers/PrimitiveHandler.scala
@@ -3,12 +3,18 @@ import mwords._
 import turbolift.abstraction.effect.Signature
 
 
-trait PrimitiveHandler[T[_[+_], +_]] {
+trait PrimitiveHandler[T[_[+_], +_]] { outer =>
   private[abstraction] def effectId: AnyRef
 
   def lift[M[+_] : MonadPar, A](ma: M[A]): T[M, A]
   def flatMap[M[+_] : MonadPar, A, B](tma: T[M, A])(f: A => T[M, B]): T[M, B]
   def zipPar[M[+_] : MonadPar, A, B](tma: T[M, A], tmb: T[M, B]): T[M, (A, B)]
+
+  final def makeMonad[M[+_] : MonadPar] : MonadPar[T[M, +?]] = new MonadPar[T[M, +?]] {
+    def pure[A](a: A): T[M, A] = outer.lift(MonadPar[M].pure(a))
+    def flatMap[A, B](tma: T[M, A])(f: A => T[M, B]): T[M, B] = outer.flatMap(tma)(f)
+    def zipPar[A, B](tma: T[M, A], tmb: T[M, B]): T[M, (A, B)] = outer.zipPar(tma, tmb)
+  }
 
   trait Decode[M[+_]] extends Signature {
     type Op[+A] = T[M, A]
