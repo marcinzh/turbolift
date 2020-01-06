553674337f78bfcff1ac7d75ae11dd5140e1e9fc
marcinzh <marcinzh1@yahoo.com>
2020-01-06 15:53:33 +0100

    micro opty

--- modules/core/src/main/scala/turbolift/abstraction/internals/interpreter/Interpreter.scala
+++ modules/core/src/main/scala/turbolift/abstraction/internals/interpreter/Interpreter.scala
@@ -19,10 +19,26 @@ final class Interpreter[M[_], U](
     new Interpreter[T[M, ?], U with V](newHead.outerMonad, newHead :: newTail, newFailEffectId)
   }
 
-  private val decoderMap: Map[AnyRef, Signature[M]] = {
-    val m: Map[AnyRef, Signature[M]] = handlerStacks.iterator.map(h => h.effectId -> h.decoder).toMap
-    val v: Signature[M] = if (failEffectId != null) m(failEffectId) else null
-    m + ((null, v))
+  def lookup(effectId: AnyRef): Signature[M] = {
+    def loop(i: Int): Signature[M] = {
+      if (vmt(i) eq effectId)
+        vmt(i+1).asInstanceOf[Signature[M]]
+      else
+        loop(i+2)
+    }
+    loop(0)
+  }
+
+  private val vmt: Array[AnyRef] = {
+    val n = handlerStacks.size
+    val arr = new Array[AnyRef]((n + 1) * 2)
+    for ((hh, i) <- handlerStacks.iterator.zipWithIndex) {
+      arr(i*2) = hh.effectId
+      arr(i*2 + 1) = hh.decoder
+    }
+    arr(n*2) = null
+    arr(n*2+1) = if (failEffectId == null) null else arr(arr.indexOf(failEffectId) + 1)
+    arr
   }
 
   private def loop[A](ua: A !! U): M[A] = {
@@ -38,8 +54,8 @@ final class Interpreter[M[_], U](
         case _ => loop(ux).flatMap(x => loop(k(x)))
       }
       case ZipPar(uy, uz) => castM(loop(uy) *! loop(uz))
-      case DispatchFO(id, op) => castS(op)(decoderMap(id))
-      case DispatchHO(id, op) => castS(op(this))(decoderMap(id))
+      case DispatchFO(id, op) => castS(op)(lookup(id))
+      case DispatchHO(id, op) => castS(op(this))(lookup(id))
       case PushHandler(uy, ph) => castM(ph.prime(push(ph.primitive).loop(uy)))
     }
   }
