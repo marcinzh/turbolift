07c99bf0de1d629aa4e04a8d30139dc87c51d0a5
marcinzh <marcinzh1@yahoo.com>
2021-01-30 22:30:14 +0100

    refac

--- modules/core/src/main/scala/turbolift/abstraction/Computation.scala
+++ modules/core/src/main/scala/turbolift/abstraction/Computation.scala
@@ -1,9 +1,6 @@
 package turbolift.abstraction
-import cats.~>
 import turbolift.abstraction.typeclass.MonadPar
 import turbolift.abstraction.internals.effect.{EffectId, AnyChoice}
-import turbolift.abstraction.internals.engine.MainLoop
-import turbolift.abstraction.internals.aux.{CanRun, CanTotallyHandle, CanPartiallyHandle}
 import turbolift.std_effects.Choice
 import HandlerCases.Primitive
 import ComputationCases._
@@ -71,31 +68,4 @@ trait ComputationInstances {
 trait ComputationExports {
   type !![+A, -U] = Computation[A, U]
   def !! = Computation
-}
-
-
-trait ComputationExtensions {
-  implicit class ComputationExtension[A, U](thiz: Computation[A, U]) {
-    def run(implicit ev: CanRun[U]): A = MainLoop.pure(ev(thiz)).run
-    def runStackUnsafe(implicit ev: CanRun[U]): A = MainLoop.pureStackUnsafe[A](ev(thiz))
-    
-    def runWith[F[_], L](h: IHandler[F, L])(implicit ev: CanTotallyHandle[U, L]): F[A] =
-      h.doHandle[A, Any](ev(thiz)).run
-
-    def runStackUnsafeWith[F[_], L, N](h: IHandler[F, L])(implicit ev: CanTotallyHandle[U, L]): F[A] =
-      h.doHandle[A, Any](ev(thiz)).runStackUnsafe
-
-    def handleWith[V] : HandleWithApply[V] = new HandleWithApply[V]
-    class HandleWithApply[V] {
-      def apply[F[_], L, N](h: Handler[F, L, N])(implicit ev: CanPartiallyHandle[V, U, L]): F[A] !! V with N =
-        h.doHandle[A, V](ev(thiz))
-    }
-
-    def downCast[V >: U] = thiz.asInstanceOf[Computation[A, V]]
-  }
-
-  implicit class ComputationOfPairExtension[A, B, U](thiz: Computation[(A, B), U]) {
-    def map2[C](f: (A, B) => C): C !! U = thiz.map(f.tupled)
-    def flatMap2[C, V](f: (A, B) => C !! V): C !! U with V = thiz.flatMap(f.tupled)
-  }
 }
--- modules/core/src/main/scala/turbolift/abstraction/Handler.scala
+++ modules/core/src/main/scala/turbolift/abstraction/Handler.scala
@@ -1,6 +1,5 @@
 package turbolift.abstraction
-import scala.util.{Try, Success, Failure}
-import cats.{Id, ~>}
+import cats.~>
 import turbolift.abstraction.internals.aux.CanPartiallyHandle
 import turbolift.abstraction.internals.interpreter.Interpreter
 
@@ -81,71 +80,4 @@ private[abstraction] object HandlerCases {
 
 trait HandlerExports {
   type IHandler[F[_], L] = Handler[F, L, Any]
-}
-
-
-trait HandlerExtensions {
-  implicit class HandlerExtension_Pair[S, L, N](val thiz: Handler[(S, *), L, N]) {
-    type Const[X] = S
-
-    def eval: Handler[Id, L, N] =
-      thiz.map(new ((S, *) ~> Id) {
-        def apply[A](pair: (S, A)): A = pair._2
-      })
-
-    def exec: Handler[Const, L, N] =
-      thiz.map[Const](new ((S, *) ~> Const) {
-        def apply[A](pair: (S, A)): S = pair._1
-      })
-
-    def justState = exec
-    def dropState = eval
-
-    def mapState[S2](f: S => S2): Handler[(S2, *), L, N] =
-      thiz.map(new ((S, *) ~> (S2, *)) {
-        def apply[A](pair: (S, A)): (S2, A) = {
-          val (s, a) = pair
-          (f(s), a)
-        }
-      })
-  }
-
-  implicit class HandlerExtension_NestedPairs[S1, S2, L, N](val thiz: Handler[Lambda[X => (S1, (S2, X))], L, N]) {
-    def joinStates: Handler[((S1, S2), *), L, N] =
-      thiz.map(new (Lambda[X => (S1, (S2, X))] ~> ((S1, S2), *)) {
-        def apply[A](pairs: (S1, (S2, A))): ((S1, S2), A) = {
-          val (s1, (s2, a)) = pairs
-          ((s1, s2), a)
-        }
-      })
-  }
-
-  implicit class HandlerExtension_Option[L, N](thiz: Handler[Option, L, N]) {
-    def toEither[E](e : => E): Handler[Either[E, *], L, N] =
-      thiz.map(new (Option ~> Either[E, *]) {
-        override def apply[A](result: Option[A]): Either[E, A] = result.toRight(e)
-      })
-
-    def toTry(e: => Throwable): Handler[Try, L, N] = 
-      thiz.map(new (Option ~> Try) {
-        override def apply[A](result: Option[A]): Try[A] = result.fold[Try[A]](Failure(e))(Success(_))
-      })
-  }
-
-  implicit class HandlerExtension_Either[E, L, N](thiz: Handler[Either[E, *], L, N]) {
-    def toOption: Handler[Option, L, N] =
-      thiz.map(new (Either[E, *] ~> Option) {
-        override def apply[A](result: Either[E, A]): Option[A] = result.toOption
-      })
-
-    def toTry(implicit ev: E <:< Throwable): Handler[Try, L, N] = 
-      thiz.map(new (Either[E, *] ~> Try) {
-        override def apply[A](result: Either[E, A]): Try[A] = result.toTry
-      })
-
-    def mapLeft[E2](f: E => E2): Handler[Either[E2, *], L, N] =
-      thiz.map(new (Either[E, *] ~> Either[E2, *]) {
-        override def apply[A](result: Either[E, A]): Either[E2, A] = result.swap.map(f).swap
-      })
-  }
 }
