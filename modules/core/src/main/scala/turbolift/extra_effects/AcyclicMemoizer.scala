f31aff1a00a56ccfe3ff7a90c08846df3ad829e4
marcinzh <marcinzh1@yahoo.com>
2021-03-08 15:19:08 +0100

    add fix() to memoizers

--- modules/core/src/main/scala/turbolift/extra_effects/AcyclicMemoizer.scala
+++ modules/core/src/main/scala/turbolift/extra_effects/AcyclicMemoizer.scala
@@ -12,7 +12,12 @@ trait AcyclicMemoizer[K, V] extends Effect[AcyclicMemoizerSig[*, K, V]] {
   final def memo[U](f: K => V !! U)(k: K): V !! U with this.type = embedHO[U](_.memo(f)(k))
   final def get: Map[K, V] !! this.type = embedFO(_.get)
 
-  final def apply[U](f: K => V !! U)(k: K) = memo(f)(k)
+  final def apply[U](f: K => V !! U): K => V !! U with this.type = memo(f)(_)
+
+  final def fix[U <: this.type](f: (K => V !! U) => (K => V !! U)): K => V !! U = {
+    def recur(k: K): V !! U = memo(f(recur))(k)
+    recur
+  }
 
   def handler: ThisIHandler[Id] = AcyclicMemoizerHandler[K, V, this.type](this)
 }
