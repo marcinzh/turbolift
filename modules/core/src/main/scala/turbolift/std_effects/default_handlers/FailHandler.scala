3289915d901c0157c1f54e654bdba353ac1abc36
marcinzh <marcinzh@tutanota.com>
2022-05-19 00:19:23 +0200

    misc

---
+++ modules/core/src/main/scala/turbolift/std_effects/default_handlers/FailHandler.scala
@@ -0,0 +1,35 @@
+package turbolift.std_effects.default_handlers
+import turbolift.!!
+import turbolift.typeclass.MonadPar
+import turbolift.typeclass.Syntax._
+import turbolift.std_effects.{Fail, FailSig}
+
+
+private[std_effects] object FailHandler:
+  def apply[Fx <: Fail](fx: Fx): fx.ThisHandler.Free[Option] =
+    new fx.Stateless[Option] with FailSig:
+      override def onReturn[A](a: A): Option[A] = Some(a)
+
+      override def onFlatMap[A, B, M[_]: MonadPar](tma: M[Option[A]])(f: A => M[Option[B]]): M[Option[B]] =
+        tma.flatMap {
+          case Some(a) => f(a)
+          case None => MonadPar[M].pure(None)
+        }
+
+      override def onProduct[A, B, M[_]: MonadPar](tma: M[Option[A]], tmb: M[Option[B]]): M[Option[(A, B)]] =
+        (tma *! tmb).map {
+          case (Some(a), Some(b)) => Some((a, b))
+          case _ => None
+        }
+
+      override def fail: Nothing !@! ThisEffect =
+        kk ?=> kk.outer(None)
+
+      override def orElse[A, U <: ThisEffect](lhs: A !! U, rhs: => A !! U): A !@! U =
+        kk ?=> kk.locally(lhs).flatMap { xs =>
+          if xs.isDefined
+          then kk.outer(xs)
+          else kk.locally(rhs)
+        }
+      
+    .toHandler
