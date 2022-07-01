95d77c71f622218b0cbb64da2ef5fabc45be1120
marcinzh <marcinzh@tutanota.com>
2022-07-01 13:09:35 +0200

    fix Effect.Combine typing

--- modules/core/src/main/scala/turbolift/Effect.scala
+++ modules/core/src/main/scala/turbolift/Effect.scala
@@ -29,38 +29,40 @@ trait Effect[Z <: Signature] extends ProtoEffect[Z]:
 
 
 object Effect:
-  private[turbolift] abstract class Combine[Fx](val sigs: Signature*):
+  private[turbolift] abstract class Combine(val sigs: Signature*):
+    //// Surprisingly, it type checks better as type member, rather than type parameter
+    protected type CombinedFx
+    protected type CombinedSig
+
     sealed trait ThisInterpreter extends IC.Unsealed:
       final override val signatures: Array[Signature] = sigs.toArray
-      final override type ThisEffect = Fx
+      final override type ThisEffect = CombinedFx
 
-    final type ThisHandler[F[+_], N] = Handler[F, Fx, N]
+    final type ThisHandler[F[+_], N] = Handler[F, CombinedFx, N]
     object ThisHandler:
-      type Id[N] = Handler.Id[Fx, N]
-      type Const[T, N] = Handler.Const[T, Fx, N]
-      type Free[F[+_]] = Handler.Free[F[+_], Fx]
-      type FreeId = Handler.FreeId[Fx]
-      type FreeConst[T] = Handler.FreeConst[T, Fx]
+      type Id[N] = Handler.Id[CombinedFx, N]
+      type Const[T, N] = Handler.Const[T, CombinedFx, N]
+      type Free[F[+_]] = Handler.Free[F[+_], CombinedFx]
+      type FreeId = Handler.FreeId[CombinedFx]
+      type FreeConst[T] = Handler.FreeConst[T, CombinedFx]
+
+    abstract class Stateless[F[+_]: Functor] extends IC.Stateless[F] with ThisInterpreter { thiz: CombinedSig => }
+    abstract class Stateful[S, F[+_]: Functor] extends IC.Stateful[S, F] with ThisInterpreter { thiz: CombinedSig => }
+    abstract class Proxy[Fx] extends IC.ProxyWithParam[Fx] with ThisInterpreter { thiz: CombinedSig => }
 
 
-  final class Combine2[Fx1 <: Stub, Fx2 <: Stub](val fx1: Fx1, val fx2: Fx2) extends Combine[Fx1 & Fx2](fx1, fx2):
-    private type Z = fx1.ThisSignature & fx2.ThisSignature
-    abstract class Stateless[F[+_]: Functor] extends IC.Stateless[F] with ThisInterpreter { thiz: Z => }
-    abstract class Stateful[S, F[+_]: Functor] extends IC.Stateful[S, F] with ThisInterpreter { thiz: Z => }
-    abstract class Proxy[Fx] extends IC.ProxyWithParam[Fx] with ThisInterpreter { thiz: Z => }
+  final class Combine2[Fx1 <: Stub, Fx2 <: Stub](val fx1: Fx1, val fx2: Fx2) extends Combine(fx1, fx2):
+    override protected type CombinedFx = fx1.type & fx2.type
+    override protected type CombinedSig = fx1.ThisSignature & fx2.ThisSignature
     def &![Fx3 <: Stub](fx3: Fx3) = new Combine3[Fx1, Fx2, Fx3](fx1, fx2, fx3)
 
 
-  final class Combine3[Fx1 <: Stub, Fx2 <: Stub, Fx3 <: Stub](val fx1: Fx1, val fx2: Fx2, val fx3: Fx3) extends Combine[Fx1 & Fx2 & Fx3](fx1, fx2, fx3):
-    private type Z = fx1.ThisSignature & fx2.ThisSignature & fx3.ThisSignature
-    abstract class Stateless[F[+_]: Functor] extends IC.Stateless[F] with ThisInterpreter { thiz: Z => }
-    abstract class Stateful[S, F[+_]: Functor] extends IC.Stateful[S, F] with ThisInterpreter { thiz: Z => }
-    abstract class Proxy[Fx] extends IC.ProxyWithParam[Fx] with ThisInterpreter { thiz: Z => }
+  final class Combine3[Fx1 <: Stub, Fx2 <: Stub, Fx3 <: Stub](val fx1: Fx1, val fx2: Fx2, val fx3: Fx3) extends Combine(fx1, fx2, fx3):
+    override protected type CombinedFx = fx1.type & fx2.type & fx3.type
+    override protected type CombinedSig = fx1.ThisSignature & fx2.ThisSignature & fx3.ThisSignature
     def &![Fx4 <: Stub](fx4: Fx4) = new Combine4[Fx1, Fx2, Fx3, Fx4](fx1, fx2, fx3, fx4)
 
 
-  final class Combine4[Fx1 <: Stub, Fx2 <: Stub, Fx3 <: Stub, Fx4 <: Stub](val fx1: Fx1, val fx2: Fx2, val fx3: Fx3, val fx4: Fx4) extends Combine[Fx1 & Fx2 & Fx3 & Fx4](fx1, fx2, fx3, fx4):
-    private type Z = fx1.ThisSignature & fx2.ThisSignature & fx3.ThisSignature & fx4.ThisSignature
-    abstract class Stateless[F[+_]: Functor] extends IC.Stateless[F] with ThisInterpreter { thiz: Z => }
-    abstract class Stateful[S, F[+_]: Functor] extends IC.Stateful[S, F] with ThisInterpreter { thiz: Z => }
-    abstract class Proxy[Fx] extends IC.ProxyWithParam[Fx] with ThisInterpreter { thiz: Z => }
+  final class Combine4[Fx1 <: Stub, Fx2 <: Stub, Fx3 <: Stub, Fx4 <: Stub](val fx1: Fx1, val fx2: Fx2, val fx3: Fx3, val fx4: Fx4) extends Combine(fx1, fx2, fx3, fx4):
+    override protected type CombinedFx = fx1.type & fx2.type & fx3.type & fx4.type
+    override protected type CombinedSig = fx1.ThisSignature & fx2.ThisSignature & fx3.ThisSignature & fx4.ThisSignature
