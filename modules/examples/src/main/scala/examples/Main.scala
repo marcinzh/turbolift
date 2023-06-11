a40cf747f7f2a4ee8571641cf3b1593ecf1c344e
marcinzh <marcinzh@tutanota.com>
2023-06-11 16:49:52 +0200

    add example

--- modules/examples/src/main/scala/examples/Main.scala
+++ modules/examples/src/main/scala/examples/Main.scala
@@ -15,6 +15,7 @@ object Main:
   val cases = List(
     HandlerShadowing,
     ReentrantRun,
+    PingPong,
   )
 
   def describe() =
---
+++ modules/examples/src/main/scala/examples/PingPong.scala
@@ -0,0 +1,66 @@
+package examples
+import turbolift.{!!, Signature, Effect}
+import turbolift.Extensions._
+import turbolift.effects.{State, Console}
+
+
+case object PingPong extends Example:
+  override def description: String = """
+    Two mutually dependent effects.
+    Example adapted from "Handling Bidirectional Control Flow" paper.
+  """
+
+  //===== Effects =====
+
+  trait PingSignature extends Signature:
+    def ping: Unit !@! (ThisEffect & Pong)
+
+  trait PongSignature extends Signature:
+    def pong: Unit !@! (ThisEffect & Ping)
+
+  case object Ping extends Effect[PingSignature] with PingSignature:
+    override def ping = perform(_.ping)
+
+  case object Pong extends Effect[PongSignature] with PongSignature:
+    override def pong = perform(_.pong)
+
+  type Ping = Ping.type
+
+  type Pong = Pong.type
+
+  //===== Handlers =====
+
+  extension (fx: Ping)
+    def ponger =
+      new fx.Proxy[Console] with PingSignature:
+        override def ping: Unit !@! (ThisEffect & Pong) =
+          k =>
+            Console.println(s"${Console.CYAN}ping${Console.RESET}") &&!
+            k.escape(Pong.pong)
+      .toHandler
+
+  extension (fx: Pong)
+    def pinger(limit: Int) =
+      case object S extends State[Int]
+      new fx.Proxy[Console & S.type] with PongSignature:
+        override def pong: Unit !@! (ThisEffect & Ping) =
+          k => 
+            for
+              i <- S.modifyGet(_ + 1)
+              _ <- Console.println(s"${Console.MAGENTA}pong $i${Console.RESET}")
+              _ <- 
+                if i == limit
+                then !!.unit
+                else k.escape(Ping.ping)
+            yield ()
+      .toHandler
+      .partiallyProvideWith[Console](S.handler(0).dropState)
+
+  //===== Run =====
+
+  override def apply(): Unit =
+    Ping.ping
+    .handleWith(Pong.pinger(10))
+    .handleWith(Ping.ponger)
+    .handleWith(Console.handler)
+    .unsafeRun
