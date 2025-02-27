b19656ac00501e40a7d526d77fd26f665757ea56
marcinzh <marcinzh@tutanota.com>
2025-02-27 16:11:49 +0100

    add effectful updates to Local

--- modules/core/src/main/scala/turbolift/interpreter/Local.scala
+++ modules/core/src/main/scala/turbolift/interpreter/Local.scala
@@ -33,3 +33,13 @@ final class Local[S, V] private[interpreter] (private val prompt: Prompt):
   inline def updateGet[A](inline f: S => (A, S)): (A, S) !! V = update(s => { val xs2 @ (_, s2) = f(s); (xs2, s2) })
   inline def getUpdate[A](inline f: S => (A, S)): (A, S) !! V = update(s => { val (x, s2) = f(s); ((x, s), s2) })
   inline def getUpdateGet[A](inline f: S => (A, S)): (A, S, S) !! V = update(s => { val (x, s2) = f(s); ((x, s, s2), s2) })
+
+  def getsEff[A, U <: V](f: S => A !! U): A !! U = get.flatMap(f)
+  inline def modifyEff[U <: V](inline f: S => S !! U): Unit !! U = get.flatMap(f).flatMap(put)
+  inline def modifyGetEff[U <: V](inline f: S => S !! U): S !! U = get.flatMap(f).flatMap(s2 => put(s2).as(s2))
+  inline def getModifyEff[U <: V](inline f: S => S !! U): S !! U = get.flatMap(s => f(s).flatMap(s2 => put(s2).as(s)))
+  inline def getModifyGetEff[U <: V](inline f: S => S !! U): (S, S) !! U = get.flatMap(s => f(s).flatMap(s2 => put(s2).as((s, s2))))
+  inline def updateEff[A, U <: V](inline f: S => (A, S) !! U): A !! U = get.flatMap(f).flatMap { case (a, s2) => put(s2).as(a) }
+  inline def updateGetEff[A, U <: V](inline f: S => (A, S) !! U): (A, S) !! U = get.flatMap(f).flatMap { case a_s2@(_, s2) => put(s2).as(a_s2) }
+  inline def getUpdateEff[A, U <: V](inline f: S => (A, S) !! U): (A, S) !! U = get.flatMap(s => f(s).flatMap { case (a, s2) => put(s2).as((a, s)) })
+  inline def getUpdateGetEff[A, U <: V](inline f: S => (A, S) !! U): (A, S, S) !! U = get.flatMap(s => f(s).flatMap { case (a, s2) => put(s2).as((a, s, s2)) })
